974fc6a06baf5d23250fe99977e11f53e19eded4
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-12-31 03:01:06 +0530

    Request body parsing implementation

---
+++ scripts/body-parsing.js
@@ -0,0 +1,29 @@
+// Request body parser
+
+var express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser'), // Include body parsing middleware
+	app = express();
+
+app.use(bodyParser.urlencoded({ extended: false })); // Call body parser with options
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html');
+});
+
+app.get('/getData', function(req, res) {
+	res.json({
+		id: 63
+	});
+});
+
+// Post request with body params sent back via response
+app.post('/setData', function(req, res) {
+	res.status(200).send(req.body.id);
+});
+
+app.listen(8000, function() {
+	console.log('Server running...');
+});
+

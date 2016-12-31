dd8f80cfbd99b28e9b12402949aa6e8c8a4b94a7
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-12-31 23:59:36 +0530

    Is saal ka last commit

--- apps/router-app/router.js
+++ apps/router-app/router.js
@@ -4,11 +4,7 @@ var userController = require('./user-controller'),
 	dataController = require('./data-controller');
 
 module.exports = function(app) {
-	app.use('/user', function(req, res, next) {
-		next();
-	}, userController);
+	app.use('/user', userController);
 
-	app.use('/data', function(req, res, next) {
-		next();
-	}, dataController);
+	app.use('/data', dataController);
 };

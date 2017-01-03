9d8219341c931445074a002f4771fb7079eecc40
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-01-03 23:18:59 +0530

    App re-structured

--- apps/CRUD-app/app.js
+++ apps/CRUD-app/app.js
@@ -1,27 +1,38 @@
 // Quotes CRUD app
 
 var express = require('express'),
-	bodyParser = require('body-parser'),
-	MongoClient = require('mongodb').MongoClient,
-	commonConsts = require('./config/commonConsts'),
-	app = express();
+    bodyParser = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
+    commonConsts = {
+    	database: {
+	        username: 'admin',
+	        password: '123456',
+	        url: '@ds011830.mlab.com:11830/amd-quotes-app'
+	    }
+    },
+    app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var dbUrl = 'mongodb://'+ commonConsts.database.username +':'+ commonConsts.database.password + commonConsts.database.url;
+var dbUrl = 'mongodb://' + commonConsts.database.username + ':' + commonConsts.database.password + commonConsts.database.url;
 
 console.log('Connecting to Database...');
 MongoClient.connect(dbUrl, function(err, db) {
-	if(err) {
-		console.log('Cannot connect to Database...');
-		console.log(err);
-		return;
-	}
+    if (err) {
+        console.log('Cannot connect to Database...');
+        console.log(err);
+        return;
+    }
 
-	require('./router')(app);
-
-	app.listen(8000, function() {
-		console.log('Database connected!');
+    app.get('/', function(req, res) {
+		res.redirect('/quotes');
 	});
-});
 
+	app.get('/quotes', function(req, res) {
+		res.send('quotes');
+	});
+
+    app.listen(8000, function() {
+        console.log('Database connected!');
+    });
+});
--- apps/CRUD-app/config/commonConsts.js
+++
@@ -1,9 +0,0 @@
-// Common app constants 
-
-module.exports = {
-	database: {
-		username: 'admin',
-		password: '123456',
-		url: '@ds011830.mlab.com:11830/amd-quotes-app'
-	}
-};

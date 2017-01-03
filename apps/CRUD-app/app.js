27066f7ae03aca270a7a16d10506bd178a9cec4f
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-01-04 00:17:25 +0530

    Frontend assets added

--- apps/CRUD-app/app.js
+++ apps/CRUD-app/app.js
@@ -2,19 +2,20 @@
 
 var express = require('express'),
     bodyParser = require('body-parser'),
+    path = require('path'),
     MongoClient = require('mongodb').MongoClient,
-    commonConsts = {
-    	database: {
-	        username: 'admin',
-	        password: '123456',
-	        url: '@ds011830.mlab.com:11830/amd-quotes-app'
-	    }
+    database = {
+        username: 'admin',
+        password: '123456',
+        url: '@ds011830.mlab.com:11830/amd-quotes-app'
     },
     app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var dbUrl = 'mongodb://' + commonConsts.database.username + ':' + commonConsts.database.password + commonConsts.database.url;
+var dbUrl = 'mongodb://' + database.username + ':' + database.password + database.url,
+    DB;
 
 console.log('Connecting to Database...');
 MongoClient.connect(dbUrl, function(err, db) {
@@ -24,15 +25,21 @@ MongoClient.connect(dbUrl, function(err, db) {
         return;
     }
 
-    app.get('/', function(req, res) {
-		res.redirect('/quotes');
-	});
-
-	app.get('/quotes', function(req, res) {
-		res.send('quotes');
-	});
+    DB = db;
 
     app.listen(8000, function() {
         console.log('Database connected!');
     });
+});
+
+app.get('/', function(req, res) {
+    res.redirect('/quotes');
+});
+
+app.get('/quotes', function(req, res) {
+    res.sendFile('index.html');
+});
+
+app.post('/createQuote', function(req, res) {
+    console.log(req.body);
 });

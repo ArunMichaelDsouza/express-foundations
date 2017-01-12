dbcb627516fb3d1873d38314a97152ad2c639e84
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-01-13 01:33:42 +0530

    Mongo data added to express view

--- apps/mongo-app/app.js
+++ apps/mongo-app/app.js
@@ -1,15 +1,29 @@
 // Mongo DB app
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+	express = require('express'),
+	app = express();
 
-// Connecting to mongodb local server
-MongoClient.connect('mongodb://localhost:27017/users')
-	.then(function(DB) {
-		return DB.collection('list').find({}).toArray();
-	})
-	.then(function(userList) {
-		console.log(userList);
-	})
-	.catch(function(err) {
-		console.log(err);
-	});
+app.set('view engine', 'jade');
+app.set('views', __dirname+'/views');
+
+app.get('/', function(req, res) {
+
+	// Connecting to mongodb local server
+	MongoClient.connect('mongodb://localhost:27017/users')
+		.then(function(DB) {
+			return DB.collection('list').find({}).toArray();
+		})
+		.then(function(userList) {
+			res.render('index', {
+				users: userList
+			})
+		})
+		.catch(function(err) {
+			res.send('Some error occurred!');
+		});
+});
+
+app.listen(8000, function() {
+	console.log('Server running...');
+});

f63e6a21b4c85f8696833a023cc9e0ef5f7a8593
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-01-13 02:12:13 +0530

    GET request param and query implementation

--- apps/mongo-app/app.js
+++ apps/mongo-app/app.js
@@ -1,29 +1,56 @@
 // Mongo DB app
 
 var MongoClient = require('mongodb').MongoClient,
-	express = require('express'),
-	app = express();
+    express = require('express'),
+    app = express(),
+    DB;
 
 app.set('view engine', 'jade');
-app.set('views', __dirname+'/views');
+app.set('views', __dirname + '/views');
 
+console.log('Connecting to database...');
+
+// Connecting to mongodb local server
+MongoClient.connect('mongodb://localhost:27017/users')
+    .then(function(db) {
+        DB = db;
+
+        app.listen(8000, function() {
+		    console.log('Database connected!');
+		});
+    })
+    .catch(function(err) {
+        res.send('Some error occurred!');
+    });
+
+// Root route request
 app.get('/', function(req, res) {
-
-	// Connecting to mongodb local server
-	MongoClient.connect('mongodb://localhost:27017/users')
-		.then(function(DB) {
-			return DB.collection('list').find({}).toArray();
-		})
-		.then(function(userList) {
+	return DB.collection('list').find().toArray()
+		.then(function(users) {
 			res.render('index', {
-				users: userList
-			})
+				users: users
+			});
 		})
-		.catch(function(err) {
+		.catch(function() {
 			res.send('Some error occurred!');
 		});
 });
 
-app.listen(8000, function() {
-	console.log('Server running...');
+// User info route
+app.get('/user/:user', function(req, res) {
+	return DB.collection('list').findOne({name: req.params.user})
+		.then(function(user) {
+			var id = req.query.id === 'true' ? user._id : null,
+				uid = req.query.uid === 'true' ? user.uid : null;
+
+			user ? res.render('user', {
+				name: user.name,
+				id: id,
+				uid: uid
+			}) : res.send('User not found!');
+		})
+		.catch(function() {
+			res.send('Some error occurred!');
+		});	
 });
+
